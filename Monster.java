02e47a2c794d7fe739da32f5d7c1656f0aaaa36c
Francesca Castleton <csinocruz@gmail.com>
2020-06-12 22:39:18 -0700

    added mood manipulators and makeOlder func

--- Monster.java
+++ Monster.java
@@ -2,18 +2,30 @@ public class Monster {
     private String name;
     private int lifespan;
     private int age;
+    private int mood; // (2 = joyful, 1 = happy, 0 = neutral, -1 = sad, -2 = angry)
 
+    // default constructor
     public Monster()
     {
         name = "(no name)";
         lifespan = 12;
     }
 
+    // overloaded constructor
     public Monster(String newName, int newLifespan)
     {
         name = newName;
         lifespan = newLifespan;
         age = 0;
+    }
+
+    private void makeOlder()
+    {
+        age++;
+        if(age>=lifespan)
+        {
+            name = "Zombie" + name;
+        }
     }
 
     public int getAge()
@@ -25,4 +37,22 @@ public class Monster {
     {
         return lifespan;
     }
+
+    public void poke()
+    {
+        if(mood>0)
+        {
+            mood--;
+        }
+    }
+
+    public void pet()
+    {
+        if(mood<4)
+        {
+            mood++;
+        }
+    }
+
+
 }
